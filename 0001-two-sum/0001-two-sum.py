a08352c394832f2cc1f0e08486c36f2c833f67ec
Sethuram2003 <64408619+Sethuram2003@users.noreply.github.com>
2025-03-31 16:59:50 -0400

    Time: 593 ms (45.7%), Space: 13.3 MB (49.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,11 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        hash_map={}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if(i!=j):
-                    if nums[i]+nums[j]==target:
-                        return (i,j)
+            if target-nums[i] in hash_map.keys():
+                return (i,hash_map[target-nums[i]])
+            else:
+                hash_map[nums[i]]=i
+        
+
+        
+        
+            
 
 
 
